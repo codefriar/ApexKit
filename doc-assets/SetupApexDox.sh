00a810370f37fd4f042f7e5f50793ed1559906f8
David Schach <dschach@x2od.com>
2023-09-28 11:31:49 -0700

    docs: update ApexDox

--- doc-assets/SetupApexDox.sh
+++ doc-assets/SetupApexDox.sh
@@ -1,3 +1,14 @@
+FILE=node_modules/@highlightjs/cdn-assets/highlight.min.js
+if test -f "$FILE"; then
+	cat node_modules/@highlightjs/cdn-assets/highlight.min.js > doc-assets/highlight.js
+	echo "\n" >> doc-assets/highlight.js
+	cat node_modules/highlightjs-apex/dist/apex.min.js >> doc-assets/highlight.js
+else
+	curl -L "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js" -o "doc-assets/highlight.js"
+	echo >> doc-assets/highlight.js
+	curl -L "https://cdn.jsdelivr.net/npm/highlightjs-apex/dist/apex.min.js" >> "doc-assets/highlight.js"
+fi
+
 printf '<link href="assets/styling.css" rel="stylesheet" />' > "doc-assets/files/main.html"
 echo >> "doc-assets/files/main.html"
 npx marked -i README.md --gfm >> "doc-assets/files/main.html"
