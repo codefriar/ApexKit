b68993cf9e093d8634633c93b7febdb201c5a687
David Schach <636977+dschach@users.noreply.github.com>
2023-09-28 09:50:44 -0700

    build(deps-dev): plugin updates (#63)
    * docs(ApexDox): update ApexDox d/t Prettier update
    
    * ci: GitHub action on pull request to test-deploy
    
    * fix: run proper task in pull request action
    
    * build(prettier): scan only org metadata in PR check
    
    * fix(build): fix sf commands on pull request
    
    * fix(ci): on PR, run tasks in parallel
    
    * fix(build): update orginit script with correct modifiers
    
    * package updates
    
    * prettier config
    
    * build(sfdx): no scratch org source tracking
    
    * style: add prettier xml config
    
    * style: include Apex new line
    
    * sync package-lock
    
    * style: run Prettier
    
    * ci: remove pull-request.yml, as another exists

--- scripts/orginit.sh
+++ scripts/orginit.sh
@@ -2,13 +2,13 @@
 
 # Install script
 echo "Cleaning previous scratch org..."
-sf org delete scratch --no-prompt --target-org=ApexKit
+sf org delete scratch --no-prompt --target-org ApexKit
 
 echo "Creating new scratch org"
-sf org create scratch --definition-file config/project-scratch-def.json --durationdays 10 -a ApexKit --nonamespace --setdefaultusername --noancestors
+sf org create scratch --definition-file config/project-scratch-def.json --duration-days 10 --alias ApexKit --no-namespace --set-default --no-ancestors
 
 echo "Push unmanaged main metadata"
-sf deploy metadata
+sf project deploy start
 
 echo "Assigning permission set"
 sf org assign permset --name Async_Log_Access
